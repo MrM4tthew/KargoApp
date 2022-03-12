fe8b15c6a118f650a4629709f6b7ff0e64ae4cb7
Muhammad Reza Firhan <81914026+cafir@users.noreply.github.com>
2022-03-12 13:51:22 +0700

    navbar

--- src/components/shipper/Index.js
+++ src/components/shipper/Index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Navbar, Container, Nav, Button, NavDropdown, Form, FormControl } from 'react-bootstrap'
 
 const Shipper = props => {
   return (
-    <div>Shipper</div>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">LMS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Shipments</Nav.Link>
+            </Nav>
+            <Navbar.Collapse className="justify-content-end">
+      <Navbar.Text>
+        Signed in as: <a href="#login">Mark Otto</a>
+      </Navbar.Text>
+    </Navbar.Collapse>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 
