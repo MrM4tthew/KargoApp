909a577675c92ca75475b44b52fed0375ba9e989
shifair <farisnura@gmail.com>
2022-03-12 13:40:13 +0700

    Setting up table in Transporter Menu for list of trucks

--- src/components/transporter/Index.js
+++ src/components/transporter/Index.js
@@ -1,12 +1,74 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import MaterialTable from "material-table";
+import { trucks } from "mock/trucks";
+import Select from 'react-select';
+import {map} from 'lodash'
 
-const Transporter = props => {
+const Transporter = (props) => {
+  const [isOpen, setOpenModal] = useState(false);
+
+  const newArr = map(trucks.data, "truck_type_name")
+  const optionTruckTypes = newArr.map((el) => ({value: el.toLowerCase(), label: el}))
+
   return (
-    <div>Transporter</div>
-  )
-}
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">LMS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Shipments</Nav.Link>
+              <Nav.Link href="#link">Trucks</Nav.Link>
+              <Nav.Link href="#link">Drivers</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Button>Logout</Button>
+        </Container>
+      </Navbar>
+      <Container className="my-5">
+        <div>
+          <h2>Transporter Menu</h2>
+        </div>
+        <div className="d-flex justify-content-between my-5">
+          <Select options={optionTruckTypes} placeholder="Truck Type" />
+        </div>
+        <div className="transporter-table-container">
+          <MaterialTable
+            title="List of Trucks"
+            columns={[
+              { title: "License Number", field: "license_number" },
+              { title: "Truck Type", field: "truck_type_name" },
+              { title: "Plate Type", field: "plate_type_name" },
+              { title: "Production Year", field: "production_year" },
+              { title: "Status", field: "status" },
+            ]}
+            data={trucks.data}
+            actions={[
+              {
+                icon: "edit",
+                tooltip: "Update Unit",
+                onClick: (event, rowData) => alert("You saved " + rowData.name),
+              },
+              {
+                icon: "add",
+                tooltip: "Add New Unit",
+                isFreeAction: true,
+                onClick: (event) => alert("You want to add a new row"),
+              },
+            ]}
+            options={{
+              actionsColumnIndex: -1,
+            }}
+          />
+        </div>
+      </Container>
+    </>
+  );
+};
 
-Transporter.propTypes = {}
+Transporter.propTypes = {};
 
-export default Transporter
+export default Transporter;
---
+++ src/mock/trucks.js
@@ -0,0 +1,33 @@
+export const trucks = {
+  result: {
+    status: "OK",
+    code: "200",
+    message: "",
+  },
+  data: [
+    {
+      id: 1,
+      license_number: "B 2021 ABC",
+      truck_type_name: "Tronton",
+      plate_type_name: "Yellow",
+      production_year: "2001",
+      status: "Active",
+    },
+    {
+      id: 2,
+      license_number: "B 2021 ABC",
+      truck_type_name: "Honda",
+      plate_type_name: "Yellow",
+      production_year: "2001",
+      status: "Active",
+    },
+    {
+      id: 3,
+      license_number: "B 2021 ABC",
+      truck_type_name: "Toyota",
+      plate_type_name: "Yellow",
+      production_year: "2001",
+      status: "Active",
+    },
+  ],
+};
